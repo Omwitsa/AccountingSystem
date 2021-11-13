42ef0e98c8baecb69b32222713e0507781a7228e
Omwitsa <omwitsawilson@yahoo.com>
2021-11-13 13:29:46 +0300

    Save Changes

---
+++ AccountingSystem/Pages/Reporting/TestReports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AccountingSystem.Pages.Reporting
+{
+    public class TestReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
